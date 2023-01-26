c53667034c595373a2a48b0378b08b702061501e
MG <alafix007@outlook.com>
2023-01-26 19:29:43 +0300

    task 2 yapıldı

--- src/Gun01/Student.java
+++ src/Gun01/Student.java
@@ -2,4 +2,10 @@ package Gun01;
 
 public class Student {
 
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba 1");
+
+        System.out.println("task 2 yapıldı");
+    }
 }
