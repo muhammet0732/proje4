8831f4fbf9ebe0e50fc8514b1c65cd51cdb7f93d
MG <alafix007@outlook.com>
2023-01-26 19:36:53 +0300

    task 3 yapıldı

--- src/Gun01/Okul.java
+++ src/Gun01/Okul.java
@@ -3,6 +3,6 @@ package Gun01;
 public class Okul {
     public static void main(String[] args) {
 
-        System.out.println("Merhaba 1");
+        System.out.println("okuldan merhaba");
     }
 }
